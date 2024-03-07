9a91993d85a79324731ea8f7fdfc28f716dc6053
AdrianEngCheeErn <adrianengcheeern@gmail.com>
2024-03-08 08:36:00 +1100

    Creation of Application and BookingSystem java files.

---
+++ src/BookingSystem.java
@@ -0,0 +1,6 @@
+public class BookingSystem {
+    public void printStatus() {
+        System.out.println("Welcome to FIT2099 Booking System");
+        System.out.println("Thank you for visiting FIT2099 Booking System!");
+    }
+}
